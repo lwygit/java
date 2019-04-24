1d9502447198141cb8933c1c5977361ae8b59696
lwygit <jyh120@outlook.com>
2019-04-24 14:38:56 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    int num;
+
+    public Main(String s) {
+        System.out.println("这条信息由构造方法输出！" + s);
+    }
+
+    public static void main(String[] args) {
+//        System.out.println("123");
+
+        //java分大小写
+        Main main = new Main("gouzao");
+//        ss();
+        String s = "小写s";
+        String S = "大写S";
+        System.out.println("s:" + s);
+        System.out.println("S:" + S);
+    }
+
+    public void ss() {
+        //java分大小写
+        String s = "小写s";
+        String S = "大写S";
+        System.out.println("s:" + s);
+        System.out.println("S:" + S);
+    }
+
+    public void setNum() {
+        num
+    }
+
+    void readme() {
+//         Java修饰符
+//
+//         访问控制修饰符 : default, public , protected, private
+//         非访问控制修饰符 : final, abstract, static, synchronized
+//
+//         变量
+//
+//         局部变量
+//         类变量（静态变量）
+//         成员变量（非静态变量）
+
+
+    }
+}
